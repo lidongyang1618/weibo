a54e9fbb66f28c4a35064fd9771f4347753bc22d
lidongyang1618 <lidongyang1618@gmail.com>
2019-02-22 02:50:07 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,11 +21,27 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
+        // 注册信息验证。
         $this->validate($request, [
-            'name'=> 'required|max:50',
-            'email'=> 'required|email|unique:users|max:255',
-            'password'=> 'required|confirmed|min:6'
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+
+        // 注册信息存储。
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+
+        // 注册成功提示信息。
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程！');
+
+
+        // 将页面重定向到用户个人信息页。
+        return redirect()->route('users.show', [$user]);
     }
 }
